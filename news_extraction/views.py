a40bbfeef951360430e0178c4daa70449b752dd7
basantachaulagain <basanta83chaulagain@gmail.com>
2018-07-19 10:46:10 +0545

    UI: added pagination in index and new pages

--- news_extraction/views.py
+++ news_extraction/views.py
@@ -1,13 +1,23 @@
-from forms import NameForm
-
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from forms import NameForm
 from .models import *
 from methods import initial_check, extract_info, save_extracted_info
 
 def index(request):
     initial_check()
-    return render(request, 'index.html',
-                  context={'news': rssdata.objects.all().order_by("-id")})
+    news_list = rssdata.objects.all().order_by("-id")
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(news_list, 5)
+    try:
+        news = paginator.page(page)
+    except PageNotAnInteger:
+        news = paginator.page(1)
+    except EmptyPage:
+        news = paginator.page(paginator.num_pages)
+    return render(request, 'index.html', {'news': news})
 
 
 def extraction(request):
@@ -24,8 +34,15 @@ def extraction(request):
         return render(request, 'extraction.html', {'form': form,
                                                    'news_id': story.pk,
                                                    'article': rssdata.objects.get(pk=story.pk)})
-
     else:
         form = NameForm()
 
     return render(request, 'extraction.html', {'form': form})
+
+
+def about_us(request):
+    return render(request, 'about_us.html')
+
+
+def contact_us(request):
+    return render(request, 'contact_us.html')
